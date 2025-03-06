fe4e9b90d02b9fc34299b6e6fa8482464029f9f0
Mark Piper <mark.piper@colorado.edu>
2025-03-06 13:34:14 -0700

    Add sample config file and code to read it

---
+++ heat/io.py
@@ -0,0 +1,11 @@
+"""Read/write to files."""
+
+import yaml
+
+
+def load_config(config_file):
+    try:
+        with open(config_file, "r") as fp:
+            return yaml.safe_load(fp).get("HeatModel", {})
+    except FileNotFoundError:
+        raise
---
+++ tests/io_test.py
@@ -0,0 +1,11 @@
+"""Test the io module."""
+
+from heat.io import load_config
+
+CONFIG_FILE = "config.yaml"
+
+
+def test_load_config(shared_datadir):
+    params = load_config(shared_datadir / CONFIG_FILE)
+    assert "n_agents" in params.keys()
+    assert params["n_agents"] == 100

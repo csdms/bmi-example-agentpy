45c7afb4bc23473477c54d84d9a1ce247833a056
Mark Piper <mark.piper@colorado.edu>
2025-03-06 15:36:46 -0700

    One last instance of HeatModel to DiffusionModel

--- diffusion/io.py
+++ diffusion/io.py
@@ -6,6 +6,6 @@ import yaml
 def load_config(config_file):
     try:
         with open(config_file, "r") as fp:
-            return yaml.safe_load(fp).get("HeatModel", {})
+            return yaml.safe_load(fp).get("DiffusionModel", {})
     except FileNotFoundError:
         raise

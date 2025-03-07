66a8d0f25094c88622fa1ed02942cbd7155fb92c
Mark Piper <mark.piper@colorado.edu>
2025-03-07 13:00:40 -0700

    Test that DiffusionModel uses input parameters

--- tests/diffusion_model_test.py
+++ tests/diffusion_model_test.py
@@ -3,6 +3,14 @@
 import agentpy as ap
 from diffusion import DiffusionModel
 
+PARAMETERS = {
+    "agents": 100,
+    "steps": 20,
+    "n_cols": 5,
+    "n_rows": 5,
+    "bmi_version": "2.0",
+}
+
 
 def test_model_is_a_diffusionmodel():
     m = DiffusionModel()
@@ -12,3 +20,11 @@ def test_model_is_a_diffusionmodel():
 def test_model_is_an_agentpy_model():
     m = DiffusionModel()
     assert isinstance(m, ap.Model)
+
+
+def test_model_has_parameters():
+    m = DiffusionModel(PARAMETERS)
+    assert m.p.agents == PARAMETERS["agents"]
+    assert m.p.steps == PARAMETERS["steps"]
+    assert m.p.n_cols == PARAMETERS["n_cols"]
+    assert m.p.n_rows == PARAMETERS["n_rows"]

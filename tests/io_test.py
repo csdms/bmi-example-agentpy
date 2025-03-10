cf762979a4c32e14d32abb65aa407622244ffc7b
Mark Piper <mark.piper@colorado.edu>
2025-03-10 11:33:02 -0600

    Test for config file not found

--- tests/io_test.py
+++ tests/io_test.py
@@ -1,5 +1,7 @@
 """Test the io module."""
 
+import pytest
+
 from diffusion.io import load_config
 
 CONFIG_FILE = "config.yaml"
@@ -9,3 +11,8 @@ def test_load_config(shared_datadir):
     params = load_config(shared_datadir / CONFIG_FILE)
     assert "agents" in params.keys()
     assert params["agents"] == 100
+
+
+def test_load_config_fails_on_unknown_file():
+    with pytest.raises(FileNotFoundError):
+        params = load_config("this-file-does-not-exist.yaml")

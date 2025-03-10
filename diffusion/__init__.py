16c343505bdf78c2c60b51ee5444bec9aad5dbb5
Mark Piper <mark.piper@colorado.edu>
2025-03-10 12:37:21 -0600

    Include load_config in top-level package

--- diffusion/__init__.py
+++ diffusion/__init__.py
@@ -1,5 +1,6 @@
 """Model diffusion over a 2D domain."""
 
 from .diffusion_model import DiffusionModel
+from .io import load_config
 
-__all__ = ["DiffusionModel"]
+__all__ = ["DiffusionModel", "load_config"]
